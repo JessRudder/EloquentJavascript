80765682a8aaca5fdc38af14cbf09f7b35ffdf9b
JessRudder <thejessicarudder@gmail.com>
2015-01-19 11:19:25 -0500

    Add in absolute value so isEven can handle negative numbers

--- isEven.js
+++ isEven.js
@@ -1,5 +1,5 @@
 function isEven(num) {
-  if (num === 0) {return true;}
-  else if (num === 1) {return false;}
-  else {return isEven(num-2);}
+  if (Math.abs(num) === 0) {return true;}
+  else if (Math.abs(num) === 1) {return false;}
+  else {return isEven(Math.abs(num-2));}
 }
