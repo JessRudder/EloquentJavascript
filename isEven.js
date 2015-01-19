f4d2137b7caa0b7dd6e46b90b5ccfaa551c3f89f
JessRudder <thejessicarudder@gmail.com>
2015-01-19 11:21:20 -0500

    Refactors so Math.abs(num) is only used once

--- isEven.js
+++ isEven.js
@@ -1,5 +1,6 @@
 function isEven(num) {
-  if (Math.abs(num) === 0) {return true;}
-  else if (Math.abs(num) === 1) {return false;}
-  else {return isEven(Math.abs(num-2));}
+  var num = Math.abs(num);
+  if (num === 0) {return true;}
+  else if (num === 1) {return false;}
+  else {return isEven(num-2);}
 }
