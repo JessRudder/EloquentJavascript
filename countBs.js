7f250709d69293bad861174c88f617f8bcf86dc0
JessRudder <thejessicarudder@gmail.com>
2015-01-20 18:44:54 -0500

    Recursive countBs function works but counter is defined outside of the function so it will have to be reset each time you want to use the function. Cant move it inside or it is reset with each recursive call.  TODO figure this out

--- countBs.js
+++ countBs.js
@@ -1,18 +1,17 @@
+var counter = 0;
+
 function countBs(phrase) {
-  counter = 0;
   if (phrase.length === 1) {
-    if (phrase.charAt(-1) === "B") {
+    if (phrase.charAt(0) === "B") {
       counter += 1;
-      return counter;
-    } else {
-      return counter;
     }
   } else {
-    if (phrase.charAt(-1) === "B") {
+    if (phrase.charAt(0) === "B") {
       counter += 1;
-      countBs(phrase.slice(0,-1));
+      countBs(phrase.slice(1,phrase.length));
     } else {
-      countBs(phrase.slice(0,-1));
+      countBs(phrase.slice(1,phrase.length));
     }
   }
+  return counter;
 }
