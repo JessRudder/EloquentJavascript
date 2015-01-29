6821f8e793e5a5c81ac4d8a83a6fd6a96bc07e6a
JessRudder <thejessicarudder@gmail.com>
2015-01-29 11:52:16 -0500

    Corrects ageDifference.js

--- ageDifference.js
+++ ageDifference.js
@@ -1,8 +1,3 @@
-function average(array) {
-  function plus(a, b) { return a + b; }
-  return array.reduce(plus) / array.length;
-}
-
 var byName = {};
 ancestry.forEach(function(person) {
   byName[person.name] = person;
