350dcdb26a9f58a0e35dcfc6e3afaac1449383ae
JessRudder <thejessicarudder@gmail.com>
2015-02-01 21:32:53 -0500

    Corrects numberGuesser.js

--- numberGuesser.js
+++ numberGuesser.js
@@ -1,12 +1,12 @@
 var num = Math.floor((Math.random() * 100) + 1);
 
-while (guess !== num) {
+while (guess != num) {
   var guess = prompt("Pick a number between 1 and 100");
 
   if (num > guess) {
     alert("The number is higher!");
   } else if (num < guess) {
-    alert("The numberis lower!");
+    alert("The number is lower!");
   } else {
     alert("You must have gotten it right!");
     break;
