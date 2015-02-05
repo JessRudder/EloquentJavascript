139c228a8f4d0381f2ca3f8532910a483bd8dd1f
JessRudder <thejessicarudder@gmail.com>
2015-02-05 17:06:47 -0500

    Grid functions added

--- eLife.js
+++ eLife.js
@@ -11,3 +11,18 @@ var plan = ["############################",
             "#    #                     #",
             "############################"];
 
+function Grid(width, height) {
+  this.space = new Array(width * height);
+  this.width = width;
+  this.height = height;
+}
+Grid.prototype.isInside = function(vector) {
+  return vector.x >= 0 && vector.x < this.width &&
+         vector.y >= 0 && vector.y < this.height;
+};
+Grid.prototype.get = function(vector) {
+  return this.space[vector.x + this.width * vector.y];
+};
+Grid.prototype.set = function(vector, value) {
+  this.space[vector.x + this.width * vector.y] = value;
+};
