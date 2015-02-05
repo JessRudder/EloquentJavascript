e10119a6737c44b949b951e684811974c6b6286e
JessRudder <thejessicarudder@gmail.com>
2015-02-05 17:27:24 -0500

    Adds directions object

--- eLife.js
+++ eLife.js
@@ -26,3 +26,17 @@ Grid.prototype.get = function(vector) {
 Grid.prototype.set = function(vector, value) {
   this.space[vector.x + this.width * vector.y] = value;
 };
+
+//each critter object has an act method that, when called, returns an action. An action is an object with a type property, which names the type of action the critter wants to take, for example "move". The action may also contain extra information, such as the direction the critter wants to move in.
+//When the act method is called, it is given a view object that allows the critter to inspect its surroundings.
+
+var directions = {
+  "n":  new Vector( 0, -1),
+  "ne": new Vector( 1, -1),
+  "e":  new Vector( 1,  0),
+  "se": new Vector( 1,  1),
+  "s":  new Vector( 0,  1),
+  "sw": new Vector(-1,  1),
+  "w":  new Vector(-1,  0),
+  "nw": new Vector(-1, -1)
+};
