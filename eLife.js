26d87217b6a9bddb852fa8f95a4cd86ccb0c3756
JessRudder <thejessicarudder@gmail.com>
2015-02-07 23:22:33 -0500

    Adds wall function

--- eLife.js
+++ eLife.js
@@ -80,3 +80,25 @@ function World(map, legend) {
                elementFromChar(legend, line[x]));
   });
 }
+
+//
+function charFromElement(element) {
+  if (element == null)
+    return " ";
+  else
+    return element.originChar;
+}
+
+World.prototype.toString = function() {
+  var output = "";
+  for (var y = 0; y < this.grid.height; y++) {
+    for (var x = 0; x < this.grid.width; x++) {
+      var element = this.grid.get(new Vector(x, y));
+      output += charFromElement(element);
+    }
+    output += "\n";
+  }
+  return output;
+};
+
+function Wall() {}
