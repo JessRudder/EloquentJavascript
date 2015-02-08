868c85ec40bfbae11d7126063637868a90aa7cbd
JessRudder <thejessicarudder@gmail.com>
2015-02-08 18:14:31 -0500

    Add Vector object definition

--- eLife.js
+++ eLife.js
@@ -11,6 +11,14 @@ var plan = ["############################",
             "#    #                     #",
             "############################"];
 
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+}
+Vector.prototype.plus = function(other) {
+  return new Vector(this.x + other.x, this.y + other.y);
+};
+
 function Grid(width, height) {
   this.space = new Array(width * height);
   this.width = width;
