d39bb411e3677db249aa5f0f05c3aeba2a786752
JessRudder <thejessicarudder@gmail.com>
2015-02-11 18:20:48 -0500

    Add method for viewing the given coordinates

--- eLife.js
+++ eLife.js
@@ -152,3 +152,30 @@ World.prototype.checkDestination = function(action, vector) {
       return dest;
   }
 };
+
+//look method figures out the coordinates that we are trying to look at and, if they are inside the grid, finds the character corresponding to the element that sits there
+//for coordinates outside the grid, pretend there is a wall there so creatures won't walk off the ledge
+
+function View(world, vector) {
+  this.world = world;
+  this.vector = vector;
+}
+View.prototype.look = function(dir) {
+  var target = this.vector.plus(directions[dir]);
+  if (this.world.grid.isInside(target))
+    return charFromElement(this.world.grid.get(target));
+  else
+    return "#";
+};
+View.prototype.findAll = function(ch) {
+  var found = [];
+  for (var dir in directions)
+    if (this.look(dir) == ch)
+      found.push(dir);
+  return found;
+};
+View.prototype.find = function(ch) {
+  var found = this.findAll(ch);
+  if (found.length == 0) return null;
+  return randomElement(found);
+};
