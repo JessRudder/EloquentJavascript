e5ad6c3aae6f61225865ddd78bb355f2e01dcbed
JessRudder <thejessicarudder@gmail.com>
2015-02-13 17:02:18 -0500

    Create retry.js

---
+++ retry.js
@@ -0,0 +1,12 @@
+function MultiplicatorUnitFailure() {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.5)
+    return a * b;
+  else
+    throw new MultiplicatorUnitFailure();
+}
+
+function reliableMultiply(a, b) {
+  // Your code here.
+}
