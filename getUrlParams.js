3055ee3c9b79ec00b13acadcadacbbe0cb823f4b
JessRudder <thejessicarudder@gmail.com>
2015-02-28 18:09:51 -0500

    Get URL params

---
+++ getUrlParams.js
@@ -0,0 +1,7 @@
+function getUrlVars() {
+  var map = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    map[key] = value;
+  });
+  return map;
+}
